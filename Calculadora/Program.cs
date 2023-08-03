b0b8d269cdd7e161286889233e7ba3f173834991
VLAD <VLAD@DESKTOP-N99SM51>
2023-08-03 18:05:01 -0300

    Add project files.

---
+++ Calculadora/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Calculadora
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Digite um valor: ");
+        int ValorX = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite um segundo valor: ");
+        int ValorY = int.Parse(Console.ReadLine());
+
+        int resultado = ValorX + ValorY;
+        Console.WriteLine($"Resultado igual a: {resultado}");
+    }
+}
